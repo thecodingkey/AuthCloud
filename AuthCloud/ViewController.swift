e201114ba317d5463e86b2d42a3cefe33b7614d2
Rosendo <rosenvb.global@gmail.com>
2022-11-01 19:48:04 -0600

    [ADD] - Google auth.

--- AuthCloud/ViewController.swift
+++ AuthCloud/ViewController.swift
@@ -7,22 +7,20 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseCore
+import GoogleSignIn
 
 class ViewController: UIViewController {
-    
-    
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tfPassword.isSecureTextEntry = true
     }
     
     
     func loginWithEmail(user:String, password:String) {
-        
         Auth.auth().signIn(withEmail: user, password: password){[weak self] resultAuth, error in
             if error == nil{
                 
@@ -34,6 +32,37 @@ class ViewController: UIViewController {
                 self?.present(vc, animated: true)
             }else{
                 print("::: Falló el inicio de sesión")
+            }
+        }
+    }
+    
+    
+    func loginWGoogle(){
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        
+        GIDSignIn.sharedInstance.configuration = config
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: self){
+            user, error in
+            
+            guard let authentication = user?.authentication,
+                  let token = authentication.idToken else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: token, accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential){ [weak self] authResult, error in
+                if error == nil{
+                    
+                    self?.tfPassword.text = ""
+                    self?.tfEmail.text = ""
+                    
+                    let vc = HomeViewController()
+                    vc.modalPresentationStyle = .fullScreen
+                    self?.present(vc, animated: true)
+                }else{
+                    print("::: Falló el inicio de sesión")
+                }
             }
         }
     }
@@ -53,5 +82,13 @@ class ViewController: UIViewController {
         loginWithEmail(user: user, password: password)
         
     }
+    
+    
+    @IBAction func signInWithGoogle(_ sender: Any) {
+        loginWGoogle()
+    }
+    
+    
+    
 }
 
